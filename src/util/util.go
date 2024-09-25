02312218f97558dcab259cd7fe31e0a4d944f9bf
kavi-99 <kavisathu96@gmail.com>
2024-09-25 18:17:34 +0800

    Resolve Golang syntax and linter errors (golangci-lint compliance)

--- src/util/util.go
+++ src/util/util.go
@@ -85,8 +85,9 @@ func RunCommandAndLogWithRetries(cmd *exec.Cmd, maxAttempts int) string {
 			return string(stdoutStderr)
 		}
 	}
-
-	log.Fatalf(err.Error())
+	// 25.09 update for logrus syntax error correction
+	log.Fatalf("Error occurred: %v", err)
+	// log.Fatalf(err.Error())
 	return err.Error()
 }
 
